9f83b60a5e3c7ce46f1cd34b4a75507340a5417b
meli <melatbeyene04@gmail.com>
2018-06-04 23:27:47 -0700

    Update replacemain.java

--- indexAssignment/replacemain.java
+++ indexAssignment/replacemain.java
@@ -1,1 +1,23 @@
 
+package inclassProject;
+
+public class replacemain {
+
+	public static void main(String[] args) {
+		
+		int [] num = {11,-7,3,42,3,0,14,3};
+		
+		ReplaceIndex FirstNum = new ReplaceIndex();
+		for (int n : num) {
+			FirstNum.add(n);
+		}
+		System.out.println("number list:" );
+		FirstNum.resu();
+		
+		FirstNum.replaceAll(3, 999);
+		System.out.println("number list :");
+		FirstNum.resu();
+		
+	}
+
+}
